03fe3d4655e0fd470f4754e4663243e580c1e329
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-03-30 18:06:06 +0200

    added-js-1stpart

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+{/* <div class="imgs active">
+<img src="./img/01.jpg">
+</div> */}
+
+let currentIndex = 0;
+const nextImg = document.getElementsByClassName("arrow-up")
+let img = document.getElementById("imgContainer")
+
+
+for(i = 0; i < items.length; i++){
+    img.innerHTML += `<div class="imgs active">
+    <img src="${items[i]}">
+    </div>`
+    
+}
+
