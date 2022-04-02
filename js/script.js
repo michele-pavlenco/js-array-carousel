6fdc57cf9745ce94578df3157d97213951d4c690
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-02 19:23:58 +0200

    end

--- js/script.js
+++ js/script.js
@@ -21,45 +21,124 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+/* <div class="prvw-img active">
+  <img src="./img/01.jpg" alt="">
+</div>
+*/
 
-{/* <div class="imgs active">
-<img src="./img/01.jpg">
-</div> */}
+
+let imgPrevew = ""
+let imgTemeplate = ""
+
+
 
 let currentIndex = 0;
+for (let i = 0; i < items.length; i++) {
+    let classActive = ""
+    if (i === currentIndex) {
+        classActive = " active"
+    }
+    imgTemeplate += `<div class="imgs ${classActive} ">
+     <img src="${items[i]}">
+     <div class="title">
+         <h2>"${title[i]}"</h2>
+         <p>${text[i]}</p>
+     </div>
+</div> `
+    imgPrevew +=
+        `<div class="prvw-img  ${classActive} ">
+ <img src="${items[i]}" alt="">
+ </div> `
+}
+//console.log(imgTemeplate)
+let imgContainer = document.getElementById("imgContainer")
+let prevewContainer = document.querySelector(".preview-img")
+imgContainer.innerHTML = imgTemeplate
+prevewContainer.innerHTML = imgPrevew
+
 const next = document.getElementById("next")
-let img = document.getElementById("imgContainer")
-let active = document.getElementById("active")
+const prev = document.getElementById("pre")
+
+function nextImg() {
+    const imgActive = document.querySelector(".imgs.active")
+    imgActive.classList.remove('active')
+    const prvwActive = document.querySelector(".prvw-img.active")
+    prvwActive.classList.remove('active')
+    const imgs = document.getElementsByClassName("imgs")
+    imgs[currentIndex].classList.add("active")
+    const prvw = document.getElementsByClassName("prvw-img")
+    prvw[currentIndex].classList.add("active")
+
+        if(currentIndex == 4){
+            currentIndex = 0
+        }else currentIndex++
+        
+}
+function prevImg() {
+    const imgActive = document.querySelector(".imgs.active")
+    imgActive.classList.remove('active')
+    const prvwActive = document.querySelector(".prvw-img.active")
+    prvwActive.classList.remove('active')
+    const imgs = document.getElementsByClassName("imgs")
+    imgs[currentIndex].classList.add("active")
+    const prvw = document.getElementsByClassName("prvw-img")
+    prvw[currentIndex].classList.add("active")
+
+if(currentIndex == 0){
+    currentIndex = 4
+}else currentIndex--
+}
 
 
 
 
 
-// for (i = 0; i < items.length; i++) {
-//     img.innerHTML += 
-//     `<div class="imgs ">
-//      <img src="${items[currentIndex]}">
-//      </div>`
-     
-// };
 
-// next.addEventListener('click', nextImg);
 
-// function nextImg() {
-//     if (i <= 4) {
-//         active.innerHTML = `
-//         <div class="imgs ">
-//         <img src="${items[i]}">
-//         </div>`
-//         d++
+next.addEventListener("click", nextImg)
+prev.addEventListener("click", prevImg)
 
-//     } else  (i > 4) 
-//         active.innerHTML = `
-//         <div class="imgs ">
-//         <img src="${items[0]}">
-//         </div>`
-//         i = 0
-// }
-// console.log(i)
+
+
+
+
+
+
+
+// let firstImg = document.querySelector("div.imgs")
+
+// firstImg.classList.add("active")
+
+
+
+// 
+// let img = document.getElementById("imgContainer")
+// let active = document.getElementsByClassName("active")
+
+
+
+
+
+
+
+//  next.addEventListener('click', nextImg);
+
+//  function nextImg() {
+//      if (i <= 4) {
+//          active.innerHTML = `
+//          <div class="imgs ">
+//          <img src="${items[i]}">
+//          </div>`
+//          currentIndex += 1
+
+//      } else if  (i > 4) 
+//          active.innerHTML = `
+//          <div class="imgs ">
+//          <img src="${items[0]}">
+//          </div>`
+//          i = 0
+//  }
+
+
 
 
